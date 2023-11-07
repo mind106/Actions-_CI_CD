ca4a7758093298b71585d4562b36b1c54145ea36
johnny.oxpay <vu.le@oxpayfinancial.com>
2023-11-07 14:03:23 +0700

    update

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:actions_cicd/controllers/auth_controller.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -6,16 +7,14 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 List pages = [
-  // VideoScreen(),
   // SearchScreen(),
-  // const AddVideoScreen(),
   // const ChatScreen(),
-  // ProfileScreen(uid: authController.user.uid),
-  const Center(child: Text("Video Screen")),
+
+  // VideoScreen(),
   const Center(child: Text("Search Screen")),
-  const Center(child: Text("AddVideo Screen")),
+  // const AddVideoScreen(),
   const Center(child: Text("Messages Screen")),
-  const Center(child: Text("Profile Screen")),
+  // ProfileScreen(uid: authController.user.uid),
 ];
 
 // COLORS
